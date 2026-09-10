8c5be4cce2831238e9f5f0517320c2b2d04344d4
shinhiy58@gmail.com <2601110281@gspace.kopo.ac.kr>
2026-09-10 10:58:13 +0900

    5장 함수형컴포터넌트 예제까지함

--- src/05/WelcomeList.jsx
+++ src/05/WelcomeList.jsx
@@ -5,9 +5,9 @@ import "./WelcomeList.css"; // 작성한 CSS 파일 임포트
 function WelcomeList() {
     return (
         <div className="welcome-list">
-            <Welcome name="김인공"></Welcome><br/>
-            <Welcome name="박폴리"></Welcome><br/>
-            <Welcome name="이정수"></Welcome><br/>
+            <Welcome name="김인공" colorClass="bg-1"/>
+            <Welcome name="박폴리" colorClass="bg-2"/>
+            <Welcome name="이정수" colorClass="bg-3"/>
         </div>
     );
 }
